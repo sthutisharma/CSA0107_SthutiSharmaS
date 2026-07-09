317c3ac907d94368b6def4132355c41c182378a5
sthutisharma <sthutisharma06@gmail.com>
2026-07-09 10:32:11 +0530

    Add files via upload

---
+++ IncrementDecrementOperators.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5;
+    cout<<"++a: "<<(++a)<<endl;
+    a=5;
+    cout<<"a++: "<<(a++)<<endl;
+    int b=5;
+    cout<<"--b: "<<(--b)<<endl;
+    b=5;
+    cout<<"b++: "<<(b++)<<endl;
+    return 0;
+}
