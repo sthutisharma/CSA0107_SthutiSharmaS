6235d4185c15fac44c7d91b17a72d5245d4d68e8
sthutisharma <sthutisharma06@gmail.com>
2026-07-09 09:47:52 +0530

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello world"<<endl;
+    cout<<"Name:Sthuti"<<endl;
+    cout<<"Department:CSE"<<endl;
+    cout<<"Year:2nd Year"<<endl;
+    return 0;
+}
