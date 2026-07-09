760bfdd8925864af15a068a178dd11a3ca4eb07d
sthutisharma <sthutisharma06@gmail.com>
2026-07-09 09:54:32 +0530

    Add files via upload

---
+++ Calculator.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two number:";
+    cin>>a>>b;
+    cout<<"Addition="<<a+b<<endl;
+    cout<<"Subtraction="<<a-b<<endl;
+    cout<<"Multiplication="<<a*b<<endl;
+    if (b!=0)
+        cout<<"Division="<<a/b<<endl;
+    else
+        cout<<"Division not possible";
+    return 0;
+}
