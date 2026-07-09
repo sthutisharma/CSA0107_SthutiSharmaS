ec4915e4289f409f642b3672441bd262aaf5db2c
sthutisharma <sthutisharma06@gmail.com>
2026-07-09 10:46:45 +0530

    Add files via upload

---
+++ BitwiseOperators.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=12,b=10;
+    cout<<"a&b: "<<(a&b)<<endl;
+    cout<<"a|b: "<<(a|b)<<endl;
+    cout<<"a^b: "<<(a^b)<<endl;
+    cout<<"~a: "<<(~a)<<endl;
+    cout<<"a<<1: "<<(a<<1)<<endl;
+    cout<<"a>>1: "<<(a>>1)<<endl;
+    return 0;
+}
