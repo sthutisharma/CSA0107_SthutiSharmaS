94388c88b9542bb36ef1968bc6b042b5c9a6efb9
sthutisharma <sthutisharma06@gmail.com>
2026-07-09 10:12:51 +0530

    Add files via upload

---
+++ IntegerVariableOperation.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=6;
+    long long int b=12345678L;
+    unsigned int c=23468U;
+    cout<<a <<" Size of a= "<<sizeof(a)<<endl;
+    cout<<b <<" Size of b= "<<sizeof(b)<<endl;
+    cout<<c <<" Size of c= "<<sizeof(c)<<endl;
+    return 0;
+}
