57ba8e87cb079041140bd80cad02bf381c3fdfd1
sthutisharma <sthutisharma06@gmail.com>
2026-07-09 10:23:31 +0530

    Add files via upload

---
+++ LogicalOperators.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    bool x=true,y=false;
+    cout<<"x&&y: "<<(x&&y)<<endl;
+    cout<<"x||y: "<<(x||y)<<endl;
+    cout<<"!x: "<<(!x)<<endl;
+    cout<<"!y: "<<(!y)<<endl;
+    return 0;
+}
