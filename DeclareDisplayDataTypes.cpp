96bfadae1141bd216665e59ff16101ad9a85b9b0
sthutisharma <sthutisharma06@gmail.com>
2026-07-09 10:07:14 +0530

    Add files via upload

---
+++ DeclareDisplayDataTypes.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a=10;
+    float b=3.14;
+    double c=9.99;
+    char d='A';
+    bool e=true;
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    cout<<e<<endl;
+    cout<<"SIZEOF a="<<sizeof(a)<<endl;
+    cout<<"SIZEOF b="<<sizeof(b)<<endl;
+    cout<<"SIZEOF c="<<sizeof(c)<<endl;
+    cout<<"SIZEOF d="<<sizeof(d)<<endl;
+    cout<<"SIZEOF e="<<sizeof(e)<<endl;
+    return 0;
+}
