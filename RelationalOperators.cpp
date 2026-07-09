fea86734454b9ea509c351a174b3f74f3a7c0496
sthutisharma <sthutisharma06@gmail.com>
2026-07-09 10:20:13 +0530

    Add files via upload

---
+++ RelationalOperators.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two number:"<<endl;
+    cin>>a>>b;
+    cout<<"a==b:"<<(a==b)<<endl;
+    cout<<"a!=b:"<<(a!=b)<<endl;
+    cout<<"a>b:"<<(a>b)<<endl;
+    cout<<"a<b:"<<(a<b)<<endl;
+    cout<<"a>=b:"<<(a>=b)<<endl;
+    cout<<"a<=b:"<<(a<=b)<<endl;
+}
