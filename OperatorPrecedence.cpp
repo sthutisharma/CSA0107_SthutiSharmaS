8ece612bb02e8ba9c1db1adacc2ff96d941a0326
sthutisharma <sthutisharma06@gmail.com>
2026-07-09 10:50:21 +0530

    Add files via upload

---
+++ OperatorPrecedence.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"10+2*5= "<<(10+2*5)<<endl;
+    cout<<"(10+2)*5= "<<((10+2)*5)<<endl;
+    cout<<"20/4+3*2= "<<(20/4+3*2)<<endl;
+    cout<<"20/(4+3)*2= "<<(20/(4+3)*2)<<endl;
+    cout<<"20+3.0*1= "<<(20+3.0*1)<<endl;
+    return 0;
+}
